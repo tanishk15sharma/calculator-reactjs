854bdb8052b5ba924c39b239acf6f3e4f7e4a52b
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-13 20:22:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">CALCULATOR</div>
+      <input value={value} type="text" className="inputfield" />
+
+      <div className="rowcontainer">
+        <div className="row1">
+          <button onClick={(e) => setValue(value + e.target.value)} value="1">
+            1
+          </button>
+        </div>
+
+        <div className="row1">
+          <button onClick={(e) => setValue(value + e.target.value)} value="1">
+            2
+          </button>
+        </div>
+        <div className="row1">
+          <button onClick={(e) => setValue(value + e.target.value)} value="1">
+            3
+          </button>
+        </div>
+        <div className="row1">
+          <button onClick={(e) => setValue(value + e.target.value)} value="1">
+            4
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
